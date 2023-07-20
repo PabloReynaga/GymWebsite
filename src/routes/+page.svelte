7a3fd69ff877a6529808267ea1808b863e112ae4
PabloReynaga <pablo.reynaga@bonprix.net>
2023-07-20 20:46:28 +0200

    Nav fuction added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,36 @@
 <script>
-  
+let y=0;
+
+$:innerHeight=0
+
+$: console.log(y);
+$: console.log(innerHeight)
+$: colored = ""
 
 
 </script>
+<svelte:window bind:scrollY={y} bind:innerHeight />
 <header>
       
 
-    <a href="#" class="logo">LOGO</a>      
-    
+    <a href="#home" class="logo">LOGO</a>      
     <nav class="items-nav">
-        <a href="home" class="active">Home</a>
-        <a href="about">About</a>
-        <a href="vlog">Vlog</a>
-        <a href="contact">Contact me</a>
+        
+        <a href="#home" class="active" >Home</a>
+        <a href="#about" class={colored}>About</a>
+        <a href="#vlog" class={colored}>Vlog</a>
+        <a href="#contact" class={colored}>Contact me</a>
     </nav>
     
     
 
 </header>
 <body>
-    <section id="home">home</section>
+    <section id="home">
+        <div class="home-container">
+
+        </div>
+    </section>
     <section id="about">about</section>
     <section id="vlog">vlog</section>
     <section id="contact">contact</section>
@@ -27,7 +38,6 @@
 
 <style lang="scss">
     
-
 .logo{
     font-size: 25px;
     text-decoration: none;
@@ -38,5 +48,8 @@
     color: $primary;
 }
 
+.home-container{
+    font-size: 27px;
+}
 
 </style>
