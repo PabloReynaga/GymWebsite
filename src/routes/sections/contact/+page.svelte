5ca5164885ef49e2edf1d28df471fc6ebe4f4904
PabloReynaga <pablo.reynaga@bonprix.net>
2023-07-24 16:09:05 +0200

    NavBar items colors added

---
+++ src/routes/sections/contact/+page.svelte
@@ -0,0 +1,43 @@
+<script>
+
+</script>
+<div class="container">
+   
+    <form >
+        <div class="formular">
+
+            <h1>Contact me</h1>
+            <label for="name">Name</label>
+            <input type="text" id="name" placeholder="name">
+        
+            <label for="name">Mail</label>
+            <input type="text" id="name" placeholder="name">
+        
+            <label for="name">Number</label>
+            <input type="text" id="name" placeholder="name">
+
+            <label for="name">Menssage</label>
+            <input type="text" id="name" placeholder="name">
+        </div>
+    </form>
+    
+</div>
+
+
+<style>
+*{
+    font-size: 20px;
+    margin: 0px;
+    padding: 0px;
+    font-family: 'Montserrat', sans-serif;
+}
+.formular{
+    background-color: grey;
+    display: block;
+    justify-content: left;
+   
+}
+
+
+
+</style>
