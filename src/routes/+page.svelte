41c6407359c11904218e1abe86b237082f9df82c
PabloReynaga <pablo.reynaga@bonprix.net>
2023-07-26 11:08:28 +0200

    contact me added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,97 @@
 <script>
+  
 import Contact from '..//routes/sections/contact/+page.svelte'
-let y=0;
+let y;
 
-$:innerHeight=0;
+$:innerHeight= 0;
+$:firstScreen = innerHeight-90;
+$:secondScrenn= firstScreen + firstScreen;
+$:thirdScreen= secondScrenn + firstScreen;
+$:console.log(innerHeight-70)
 
-$: console.log(y);
-$: console.log(innerHeight)
+$:console.log(y)
+
 $: colored = {
     homeButton : true, 
     aboutButton :false,
     vlogButton:false, 
-    contactMeButton : false}
+    contactMeButton : false
+}
+
+setInterval(()=>{
+        switch(true){
+        case firstScreen>y:
+            colored.homeButton = true;
+            colored.aboutButton= false;
+            colored.vlogButton = false
+            colored.contactMeButton =false;
+            break;
+        case firstScreen<y && secondScrenn>y:
+            colored.homeButton= false;
+            colored.aboutButton= true;
+            colored.vlogButton= false;
+            colored.contactMeButton=false;
+            break;
+        case secondScrenn<y && thirdScreen>y:
+            colored.homeButton=false
+            colored.aboutButton= false;
+            colored.vlogButton= true;
+            colored.contactMeButton=false
+            break;
+        case thirdScreen<y:
+            colored.homeButton = false;
+            colored.aboutButton= false;
+            colored.vlogButton= false;
+            colored.contactMeButton=true;
+            break;
+    } 
+},10)
+
 
 
 </script>
-<svelte:window bind:scrollY={y} bind:innerHeight />
+
+<svelte:window bind:scrollY={y} bind:innerHeight={innerHeight}/>
 <header>
-    <a href="#home" class="logo">LOGO</a>      
+
+    <a href="#home"  class="logo">LOGO</a>
+    
+
     <nav class="items-nav">
         <a href="#home" class:active={colored.homeButton} on:click={()=>{colored.homeButton=true, colored.aboutButton = false, colored.vlogButton = false, colored.contactMeButton= false}}>Home</a>
         <a href="#about" class:active={colored.aboutButton} on:click={()=>{colored.homeButton=false, colored.aboutButton = true, colored.vlogButton = false, colored.contactMeButton= false}}>About</a>
         <a href="#vlog" class:active={colored.vlogButton} on:click={()=>{colored.homeButton=false, colored.aboutButton = false, colored.vlogButton = true, colored.contactMeButton= false}}>Vlog</a>
         <a href="#contact" class:active={colored.contactMeButton} on:click={()=>{colored.homeButton=false, colored.aboutButton = false, colored.vlogButton = false, colored.contactMeButton= true}}>Contact me</a>
     </nav>
+
 </header>
 <body>
+    
     <section id="home">
-        <div class="home-container">Home
-
-        </div>
+        <img alt="img"  src="../main-icon.png">
+      home
     </section>
     <section id="about">about</section>
     <section id="vlog">vlog</section>
     <section id="contact" >
         <Contact/>
-
     </section>
 </body>
-
+<footer>
+    <div class="footer">
+        © Cacho Fitness Coach. All the right reserved. 2023 
+    </div>
+    
+</footer>
 <style lang="scss">
+
+.footer{
+    font-size: 10px;
+    background-color: $bg-dark-gray;
+    color: $secondary;
+    padding: 10px;
+    
+}
     
 .logo{
     font-size: 25px;
@@ -50,8 +103,5 @@ $: colored = {
     color: $primary;
 }
 
-.home-container{
-    font-size: 27px;
-}
 
 </style>
--- src/routes/sections/contact/+page.svelte
+++ src/routes/sections/contact/+page.svelte
@@ -1,43 +1,146 @@
 <script>
+    import Button from './../../../componets/button.svelte'
 
 </script>
 <div class="container">
-   
-    <form >
-        <div class="formular">
+   <div class="formular">
+            <div class="header-titulos">
+                <h1 class="title">Trabajemos juntos en tus metas!</h1>
+                <p class="p">Si te quedaron dudas contactame para ver tu situacion y perzonalizar un plan especifico para ti.</p>
+            </div>
+            <div class="container-input">
+                
+                <form action="https://formsubmit.co/reynagafarias@gmail.com" method="post">
+                    
+                <label class="label-input"  for="name">Nombre*</label>
+                <input class="input-field" type="text" id="name" required name="Nombre">
+            
+                <label class="label-input" for="mail">Mail*</label>
+                <input class="input-field" type="mail" id="mail" required name="Mail">
+            
+                <label class="label-input" for="number">Tu Whatsapp*</label>
+                <input class="input-field" type="text" id="number" required name="Whatsapp">
 
-            <h1>Contact me</h1>
-            <label for="name">Name</label>
-            <input type="text" id="name" placeholder="name">
-        
-            <label for="name">Mail</label>
-            <input type="text" id="name" placeholder="name">
-        
-            <label for="name">Number</label>
-            <input type="text" id="name" placeholder="name">
-
-            <label for="name">Menssage</label>
-            <input type="text" id="name" placeholder="name">
-        </div>
-    </form>
+                <label class="label-input" for="instagram">Tu instagram</label>
+                <input class="input-field" type="text" id="instagram" name="Instagram" >
     
+                <label class="label-input" for="massage">Cual es tu meta?</label>
+                <textarea class="text-area-field" type="text" id="massage" name="Mensaje"
+                >Hola! mi nombre es... y quiero empezar! </textarea>
+                <div class="button-container">
+                    <Button buttonValue="Enviar"></Button>
+                </div> 
+                </form>
+                
+            </div>
+
+         </div>
+            
+
 </div>
 
 
-<style>
+<style lang="scss">
+    
+
 *{
     font-size: 20px;
     margin: 0px;
     padding: 0px;
     font-family: 'Montserrat', sans-serif;
-}
-.formular{
-    background-color: grey;
-    display: block;
-    justify-content: left;
-   
+    
 }
 
+
+.formular{
+    min-height: 80vh;
+    display:block;
+    justify-content: center;
+    
+    min-width: 80vw;
+
+}
+.container-input{
+    display: block;
+    justify-content: center;
+    align-items: center;
+    box-sizing: border-box;
+    align-items: center;
+
+    
+  
+}
+
+.input-field{
+    justify-content: center;
+    display:flex;
+    text-align: left;
+    box-sizing: border-box;
+    padding: 8px;
+    margin: auto;
+    
+
+}
+.input-field:focus{
+    outline: none;
+    box-shadow:0px 0px 2px 2px $primary;
+}
+.input-field:hover{
+    box-shadow:0px 0px 2px 2px $primary;
+}
+.label-input{
+    justify-content: center;
+    display: flex;
+    margin: 20px;
+    color: $secondary;
+}
+.title{
+    display: flex;
+    justify-content: center;
+    font-size: 32px;
+    padding: 10px;
+    box-sizing: border-box;
+    
+    
+}
+.p{
+    display: flex;
+    justify-content: center;
+    padding: 25px;
+    box-sizing: border-box;
+    margin: auto;
+}
+
+.text-area-field{
+    display: block;
+    justify-content: center;
+    align-items: center;
+    margin: auto;
+    resize: none;
+    width: 40vw;
+    height: 15vh;
+    padding: 8px;
+    box-sizing: content-box;
+      
+}
+.text-area-field:hover{
+    
+    box-shadow:0px 0px 2px 2px $primary;
+    
+}
+.text-area-field:focus{
+    outline: none;
+    box-shadow:0px 0px 2px 2px $primary;
+}
+.button-container{
+    margin-bottom: 15px;
+    padding-bottom: 10px;
+}
+.header-titulos{
+    display: block;
+    justify-content: center;
+    width: 80vw;
+}
 
 
 </style>
