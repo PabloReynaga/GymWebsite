e778a11adec04b43eb0ed33b74afc32a63f8f807
PabloReynaga <reynagafarias@gmail.com>
2023-08-31 12:28:17 +0200

    iconos socialmedia

--- src/routes/sections/about/+page.svelte
+++ src/routes/sections/about/+page.svelte
@@ -1,20 +1,33 @@
 <script>
     import Img from '$lib/img/foto_about.png'
+    import IconInstagram from '$lib/icon/instagram.png'
+    import IconYouTube from '$lib/icon/youtube.png'
 
 </script>
 <div class="section-container">
     <div class="header-container">
         <h1 class= "title">Mi trasformación</h1>
-        <p class = "subtitle">Luego de enfoque y dedicacion, genere un plan
-           de coach para lograr mi transformación fisica.
+        <p class = "subtitle">Luego de enfoque y dedicacion cree un plan
+           para lograr un cambio real y duradero en mi fisico.
         </p>
     </div>
     <div>
         <img class="img" alt="img" src={Img}>
     </div>
-    <p>
+    <p class="redessociales">Sigueme en mi redes sociales 
+        para poder ver más:
     </p>
-    <button></button>
+    <div class="container-icons">
+        <button class="icon-instagram">
+            <img  alt="img" src={IconInstagram}>
+        </button>
+        <button class="icon-youtube">
+            <img  alt="img" src={IconYouTube}>
+        </button>
+    </div>
+    
+    
+    
 </div>
 
 
@@ -49,6 +62,30 @@
     }
     .img{
         padding: 20px;
+    }
+    .container-icons{
+        display: flex;
+        justify-content: center;
+    }
+    .icon-instagram{
+        width: 35px;
+        display: flex;
+        justify-content: center;
+        margin-right: 15px;
+    }
+    .icon-youtube{
+        width: 40px;
+        display: flex;
+        justify-content: center;
+        margin-right: 15px;
+    }
+    .redessociales{
+        display: flex;
+    justify-content: center;
+    padding: 25px;
+    box-sizing: border-box;
+    margin: auto;
+
     }
  
 </style>
